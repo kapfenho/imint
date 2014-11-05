6ea3178dc9fc170be16bdbbd9ac2aea45d55dc0b
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2014-11-05 18:39:03 +0100

    deplenv

---
+++ setup/idm-deplenv.sh
@@ -0,0 +1,10 @@
+# set deployment environment
+#   ${HOME}/.env/idm-deplenv.sh
+#
+. ${HOME}/.env/common.sh
+. ${HOME}/.env/idm.sh
+. ${WL_HOME}/server/bin/setWLSEnv.sh
+deploy() {
+  java weblogic.WLST -loadProperties ~/.wlst/${1}.prop ${HOME}/lib/deploy.py
+}
+
